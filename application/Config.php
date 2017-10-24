ebbfe86120a6495523cdfb753f2b0c67edb2211d
angel charlot <legna18205@gmail.com>
2017-10-24 17:48:56 -0400

    bd lista

--- application/Config.php
+++ application/Config.php
@@ -9,16 +9,16 @@
  */
 
 
-define('BASE_URL', 'http://localhost/framework/');
+define('BASE_URL', 'http://localhost/siseps/');
 //define('BASE_URL', 'http://192.168.0.101/cumanax/');
 //define('BASE_URL', 'http://americas89.no-ip.info:8080/didactico/');
 //define('DEFAULT_CONTROLLER', 'faceblocked');
 define('DEFAULT_CONTROLLER', 'principal');
 //define('DEFAULT_LAYOUT', 'layout_legna');
 define('DEFAULT_LAYOUT', 'basic');
-define('APP_NAME', 'framework');
-define('APP_SLOGAN', 'framework');
-define('APP_COMPANY', 'Tecnoservi');
+define('APP_NAME', 'siseps');
+define('APP_SLOGAN', 'siseps');
+define('APP_COMPANY', 'depart. sistema');
 define('APP_TLF', '');
 define('APP_EMAIL', '');
 define('SESSION_TIME', 1000000);
@@ -26,7 +26,7 @@ define('HASH_KEY', '4f6a6d832be79');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'framework');
+define('DB_NAME', 'siseps');
 define('DB_CHAR', 'utf8');
 
 
