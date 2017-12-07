44e03f67a16ac41ae6575cdd2e2e71690e185fed
angel charlot <legna18205@gmail.com>
2017-12-07 12:10:03 -0430

    arreglo de problemas de permisos

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 //var base_url = 'http://192.168.0.101/cumanax/';
-var base_url = 'http://localhost:82/';
+var base_url = 'http://192.168.0.60/';
 //var base_url = 'http://localhost/siseps/';
 
 $(document).on('click', '#inicio', function() {
